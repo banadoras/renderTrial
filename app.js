54733115e1d83d4ad01be3b1db22ffe06f2e1a39
banadoras <banadoras@yahoo.com>
2022-12-20 13:16:24 +0000

    Update app.js and package.json

--- app.js
+++ app.js
@@ -1,80 +1,83 @@
-require("dotenv").config()
+require("dotenv").config();
 
 //-------- required packages/files
-const express = require("express")
-const app = express()
-var cors = require('cors')
-const methodOverride = require("method-override")
-const session = require("express-session")
-const flash = require("connect-flash")
-const path = require("path")
-const MongoStore = require("connect-mongo")
-const multer = require("multer")
+const express = require("express");
+const app = express();
+var cors = require("cors");
+const methodOverride = require("method-override");
+const session = require("express-session");
+const flash = require("connect-flash");
+const path = require("path");
+const MongoStore = require("connect-mongo");
+const multer = require("multer");
 const storage = multer.diskStorage({
-    destination:(req,file,cb)=>{
-        cb(null,path.join(__dirname,"public/uploads"))
-    },
-    filename:(req,file,cb)=>{
-        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-        cb(null, file.fieldname + '-' + uniqueSuffix)
-    }
-})
-const upload = multer({storage:storage})
-const { authenticate, login } = require("./auth")
+  destination: (req, file, cb) => {
+    cb(null, path.join(__dirname, "public/uploads"));
+  },
+  filename: (req, file, cb) => {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, file.fieldname + "-" + uniqueSuffix);
+  },
+});
+const upload = multer({ storage: storage });
+const { authenticate, login } = require("./auth");
 
 //----- app set and use
 //app.set("view engine","ejs")
-const corsOptions = {origin:"https://4vpf33.csb.app",optionsSuccessStatus: 200 }
-app.use(cors(corsOptions))
-app.use(express.static(path.join(__dirname,"public")))
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use(methodOverride("_method"))
-const dburl = `mongodb+srv://banadoras:${process.env.DB_PASSWORD}@cluster0.bocrh.mongodb.net/${process.env.DB}?retryWrites=true&w=majority`
-app.use(session({
-    secret:process.env.D_PASSWORD,
-    saveUninitialized:false,
-    resave:false,
-    cookie:{},
-    store:MongoStore.create({mongoUrl:dburl})
-}))
-app.use(flash())
+const corsOptions = {
+  origin: "https://4vpf33.csb.app",
+  optionsSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+const dburl = `mongodb+srv://banadoras:${process.env.DB_PASSWORD}@cluster0.bocrh.mongodb.net/${process.env.DB}?retryWrites=true&w=majority`;
+app.use(
+  session({
+    secret: process.env.D_PASSWORD,
+    saveUninitialized: false,
+    resave: false,
+    cookie: {},
+    //store: MongoStore.create({ mongoUrl: dburl }),
+  })
+);
+app.use(flash());
 
 //------ connect to Database
-require("./databases/db")
+//require("./databases/db");
 
 //------ connecting routers
-app.use("/users",require("./routers/user"))
-app.use("/items",require("./routers/item"))
+app.use("/users", require("./routers/user"));
+app.use("/items", require("./routers/item"));
 
-
-
+app.get("/", (req, res) => {
+  res.send(`<h1>Server Running</h1>`);
+});
 
 app.get("/logout", (req, res) => {
-    req.session.destroy()
-    res.redirect("/login")
-})
-
+  req.session.destroy();
+  res.redirect("/login");
+});
 
 //----------POST requests
 
 app.post("/login", async (req, res) => {
-    try {
-        const user = await login(req.body)
-        if (user) {
-            req.session.user = user
-            res.redirect("/protected")
-        }
-    } catch (error) {
-        req.flash("errorMessage",error.message)
-        res.redirect("/login")
+  try {
+    const user = await login(req.body);
+    if (user) {
+      req.session.user = user;
+      res.redirect("/protected");
     }
-})
-
+  } catch (error) {
+    req.flash("errorMessage", error.message);
+    res.redirect("/login");
+  }
+});
 
 //---------- Server listening
-const PORT = process.env.PORT || 3000
-app.listen(PORT,()=>{
-    console.log(`Listening to port ${PORT}`)
-})
-
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Listening to port ${PORT}`);
+});
